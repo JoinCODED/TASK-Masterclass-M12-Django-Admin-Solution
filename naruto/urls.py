ba7557227c8b057f153375e817860f719a060cd1
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2022-06-22 12:49:29 +0300

    Add ninja models

--- naruto/urls.py
+++ naruto/urls.py
@@ -13,10 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from typing import Union
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import URLPattern, URLResolver, path
 
 
-urlpatterns = [
+URLPatterns = list[Union[URLPattern, URLResolver]]
+
+urlpatterns: URLPatterns = [
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
--- ninjas/models.py
+++ ninjas/models.py
@@ -1,4 +1,47 @@
 from django.db import models
 
 
-# Create your models here.
+class Village(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Academy(models.Model):
+    cover = models.ImageField()
+    village: "Village" = models.OneToOneField(
+        "ninjas.Village", on_delete=models.CASCADE, related_name="academies"
+    )
+    curriculum = models.TextField(default="", blank=True)
+    founded = models.DateField()
+
+    def __str__(self) -> str:
+        return f"{self.village}'s Ninja Academy"
+
+
+class Cohort(models.Model):
+    academy: "Academy" = models.ForeignKey(
+        "ninjas.Academy", on_delete=models.CASCADE, related_name="cohorts"
+    )
+    start_at = models.DateField()
+    end_at = models.DateField()
+
+    def __str__(self) -> str:
+        return f"{self.academy}'s Cohort"
+
+
+class Sensei(models.Model):
+    name = models.CharField(max_length=30)
+    cohorts = models.ManyToManyField("ninjas.Cohort", related_name="senseis")
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Ninja(models.Model):
+    name = models.CharField(max_length=30)
+    graduated = models.DateField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
